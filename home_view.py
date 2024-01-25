7aa157df003f74746773a1eff0cc0ca42d0cc62d
Cody Lepp <cdylpp@yahoo.com>
2024-01-25 09:34:54 -0800

    Connected verison

---
+++ home_view.py
@@ -0,0 +1,10 @@
+import sys
+from main_view import *
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
