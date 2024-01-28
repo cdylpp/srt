c657bcff1b8dbe23a0aca77ebdd709b4dfd2a1e6
Cody Lepp <cdylpp@yahoo.com>
2024-01-28 08:41:42 -0800

    Login logic

--- main.py
+++ main.py
@@ -1,22 +1,51 @@
+# Version 1.0
+# Top Level Main Application File
+# Runs QApplication in event-loop.
+# Shows QMainWindow MainWindow()
+#
+#
+#
+
 import sys
+from PyQt6.QtCore import Slot
 from PyQt6.QtWidgets import QApplication
-from login_view import LoginWindow
+from login_view import LoginDialog
 from home_view import MainWindow
+from srt_app import MySrtApp
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
+
+class SrtApp(QApplication):
+    def __init__(self):
+        super().__init__()
+
+        self.login = LoginDialog('mysql', db_url=os.getenv("DB_URL"))
+        self.home_window = MySrtApp()
+        self.user = None
+
+        login.user_signal.connect(self.get_user)
+        login.login_signal.connect(self.home_window.show)
+
+    @Slot()
+    def get_user(self):
+        self.user = self.login.get_user()
+        return
+
+
+
 print("Starting application...")
-app = QApplication([])
+srtApp = QApplication([])
 
 # Use for `mysql` database
 # AWS RDS Mysql
 # show login, the login window is the view.
-login = LoginWindow('mysql', db_url=os.getenv("DB_URL"))
+login = LoginDialog('mysql', db_url=os.getenv("DB_URL"))
 
 home = MainWindow()
 
-login.login_success.connect(home.show)
+login.login_signal.connect(home.show)
 
 login.show()
 
