7a2228974f0560aa5e142baa3ee50dd294404e60
Cody Lepp <cdylpp@yahoo.com>
2024-01-28 11:40:04 -0800

    old main, version 1

--- main.py
+++ main.py
@@ -11,27 +11,10 @@ from PyQt6.QtCore import Slot
 from PyQt6.QtWidgets import QApplication
 from login_view import LoginDialog
 from home_view import MainWindow
-from srt_app import MySrtApp
+from srt_app import MainWindow
 from dotenv import load_dotenv
 import os
 load_dotenv()
-
-
-class SrtApp(QApplication):
-    def __init__(self):
-        super().__init__()
-
-        self.login = LoginDialog('mysql', db_url=os.getenv("DB_URL"))
-        self.home_window = MySrtApp()
-        self.user = None
-
-        login.user_signal.connect(self.get_user)
-        login.login_signal.connect(self.home_window.show)
-
-    @Slot()
-    def get_user(self):
-        self.user = self.login.get_user()
-        return
 
 
 
@@ -52,4 +35,4 @@ login.show()
 # Use for `csv` database
 # login = LoginWindow('csv', file='Credentials.csv')
 
-sys.exit(app.exec())
+sys.exit(srtApp.exec())
